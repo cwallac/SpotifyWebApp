bba1f163ebd1fb7dda7907e8f8beb7fd41665f43
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2014-09-11 00:09:56 -0400

    Created the Flask framework

--- app.py
+++ app.py
@@ -4,17 +4,45 @@ This contains the Flask app for our collaborative Spotify web app
 authors: Sawyer Vaughan and Chris Wallace 
 '''
 
-from flask import Flask, render_template, redirect
+from flask import Flask
+from flask import render_template, redirect, request
+
+
+spotifyClasses = []
 
 app = Flask(__name__)
 
-@app.route('/')
-def start():
-	return render_template('start.html')
+@app.route('/', methods=['POST', 'GET'])
+def redirect():
+	return redirect('/login')
 
-@app.route('/<url>')
-def playlist(url):
-	return render_template('playlist.html')
+@app.route('/login', methods=['POST', 'GET'])
+def start():
+	return render_template('login.html')
+
+@app.route('/authenticate', methods=['POST', 'GET'])
+def authenticate():
+	x = request.form['user']
+	y = request.form['password']
+	new_user = 
+	return 'hi'
+
+@app.route('/url_not_found')
+def urlnotfound():
+	return 'Playlist not found'
+
+@app.route('/<username>', methods=['POST', 'GET'])
+def playlist(username):
+	global spotifyClasses
+	names = []
+	not_found = True
+	for entry in spotifyClasses:
+		if username == entry.name:
+			not_found = False
+			user = entry
+	if not_found:
+		return redirect('/url_not_found')
+	return render_template('playlist.html', user=user)
 
 if __name__=="__main__":
 	app.run()
